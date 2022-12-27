1e1517dcf7a39791ed0cadc130b26b8e3d56838d
ewarozalia17 <ewa.rozalia.rostek@gmail.com>
2022-12-27 18:18:33 +0100

    second commit

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ server = http.createServer(app)
 
 app.use(express.json());
 
+app.get('/',(req,res)=>{
+    console.log("log from console log");
+    res.send("Lets start ");
+})
+
 app.get('/showFirstContainer',(req,res)=>{
     console.log("log from console log");
     res.send("Hello my friend :) welcome to github ");
