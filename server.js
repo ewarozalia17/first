d75bc0722e1518153903ce075577353a626bc4e0
ewarozalia17 <ewa.rozalia.rostek@gmail.com>
2022-12-28 15:17:27 +0100

    add new process env

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const http = require('http');
 const app = express()
 const port= process.env.PORT || 8080;   //CHANGED FOR CONTAINER
 server = http.createServer(app)
+const name = process.env.name || "Piotrek";
 
 app.use(express.json());
 
 app.get('/',(req,res)=>{
     console.log("log from console log");
-    res.send("Lets start ");
+    res.send("Welcome ",name);
 })
 
 app.get('/showFirstContainer',(req,res)=>{
